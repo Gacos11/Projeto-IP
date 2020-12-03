077f7215c3d18ada3cbecdb98f4b1ebc83413a84
Gacos11 <74110243+Gacos11@users.noreply.github.com>
2020-12-03 15:53:18 +0000

    1

--- WordSearch.java
+++ WordSearch.java
@@ -10,12 +10,20 @@ public class WordSearch {
 
     public static boolean isHidden (char[][] board, String word){
 
+        boolean isHidden = false;
         for (int lin=0; lin<board.length; lin++){
-        line.toString(board[lin]);
+
+            String line = new String(board[lin]);
+            int intIndex = line.indexOf(word);
+            if (intIndex != -1){
+                isHidden = true;
+            }
 
             for (int col=0; col<board[lin].length; col++){
 
             }
         }
+
+        return isHidden;
     }
 }
