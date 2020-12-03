823638a261169968bcb2c966325b833442fc4249
Gacos11 <74110243+Gacos11@users.noreply.github.com>
2020-12-03 13:20:06 +0000

    1

---
+++ WordSearch.java
@@ -0,0 +1,21 @@
+/**
+* Compile:     javac NumberSearch.java
+* Run:         java NumberSearch
+*
+*@author Duarte Ferreira, fc54981
+*@author Vasco Barros, fc54986
+*/
+
+public class WordSearch {
+
+    public static boolean isHidden (char[][] board, String word){
+
+        for (int lin=0; lin<board.length; lin++){
+        line.toString(board[lin]);
+
+            for (int col=0; col<board[lin].length; col++){
+
+            }
+        }
+    }
+}
