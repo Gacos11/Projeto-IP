76d2f1943ee6d64f6b8569046690691bf30ff881
Gacos11 <74110243+Gacos11@users.noreply.github.com>
2020-12-04 09:11:31 +0000

    1

--- WordSearch.java
+++ WordSearch.java
@@ -1,12 +1,17 @@
 /**
-* Compile:     javac NumberSearch.java
-* Run:         java NumberSearch
+* Compile:     javac WordSearch.java
+* Run:         java WordSearch
 *
 *@author Duarte Ferreira, fc54981
 *@author Vasco Barros, fc54986
 */
 
 public class WordSearch {
+
+    public static void main (String [] args){
+        System.out.println(puzz)
+        System.out.println(isHidden())
+    }
 
     public static boolean isHidden (char[][] board, String word){
 
@@ -15,15 +20,25 @@ public class WordSearch {
 
             String line = new String(board[lin]);
             int intIndex = line.indexOf(word);
+            
             if (intIndex != -1){
                 isHidden = true;
             }
 
             for (int col=0; col<board[lin].length; col++){
+                StringBuilder column = new StringBuilder(board[col][lin]);
+                intIndex = column.indexOf(word);
 
+                if(intIndex != -1){
+                    isHidden = true;
+                }
             }
         }
 
         return isHidden;
+    }
+
+    public static boolean isValidGame (char[][] board, String[] hiddenWords){
+
     }
 }
