2e5de8dbd9d00a5471f7da065cb32b137997af08
Gacos11 <74110243+Gacos11@users.noreply.github.com>
2020-12-06 14:36:04 +0000

    1

--- WordSearch.java
+++ WordSearch.java
@@ -12,20 +12,20 @@ public class WordSearch {
     public static void main (String [] args){
         PuzzleReader puzzle = new PuzzleReader(args[0]);
         char [][] puzzle1 = puzzle.getPuzzle();
-        //System.out.println(isHidden(puzzle1, "J"));
-         for (int i=0; i<puzzle1.length; i++){
+        System.out.println(isHidden(puzzle1, "PROGRAMACAO"));
+        /* for (int i=0; i<puzzle1.length; i++){
             System.out.println(puzzle1[i]);
             for (int j=0; j<puzzle1[i].length; j++){
-                //System.out.println(puzzle1[i][j]);
+                System.out.println(puzzle1[i][j]);
             }
-        }
+        } */
     }  
 
     public static boolean isHidden (char[][] board, String word){
 
         
         boolean isHidden = false;
-        System.out.println(board[0].length);
+        //System.out.println(board[0].length);
         for (int lin=0; lin<board.length; lin++){
 
             String line = String.valueOf(board[lin]);
@@ -35,8 +35,9 @@ public class WordSearch {
                 isHidden = true;
             }
 
+
             for (int col=0; col<board[lin].length; col++){
-                StringBuilder column = new StringBuilder(board[col][lin]);
+                StringBuilder column = new StringBuilder(board[lin][col]);
                 intIndex = column.indexOf(word);
 
                 if(intIndex != -1){
