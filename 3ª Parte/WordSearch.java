646bffc16dbcd1281e13e2744c7990f90755ca4c
DuarteFerrari <duarte.mcf@gmail.com>
2020-12-29 14:18:39 +0000

    1

--- 3ª Parte/WordSearch.java
+++ 3ª Parte/WordSearch.java
@@ -1,8 +1,40 @@
 public class WordSearch {
 
     private int wordPoints;
+    private char [][] wordsFound;
     
     public WordSearch (Puzzle puzzle, int durationsInSeconds){
+        this.puzzle = puzzle;
+        this.durationsInSeconds = durationsInSeconds;
+    }
 
+    public Puzzle puzzle(){
+        return this.puzzle;
+    }
+
+    public int duration(){
+        int maxDuration = 600;
+        return maxDuration;
+    }
+
+    public int howManyFoundWords(){
+        return this.wordsFound.lenght;
+    }
+    
+    public string [] foundWords(){
+    }
+
+    public int score(){
+        return this.wordPoints;
+    }
+
+    public boolean isFinished(){
+    }
+
+    public boolean play(Move move){
+    }
+
+    public String toString(){
     }
 }
+
