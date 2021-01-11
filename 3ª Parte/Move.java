0720ece1ff5870c7c8fa881abd3bec9641d50ba7
DuarteFerrari <duarte.mcf@gmail.com>
2021-01-11 16:31:54 +0000

    1

--- 3ª Parte/Move.java
+++ 3ª Parte/Move.java
@@ -13,12 +13,18 @@ public class Move{
 	private int columns;
 	
 	/**
-	 * @param row1
-	 * @param col1
-	 * @param row2
-	 * @param col2
-	 * @param rows
-	 * @param columns
+	 * Defines wether ro1, col1, ro2, col2, are columns and rows from the board and form
+	 * a continuous line
+	 * 
+	 * @param row1 a row from the board
+	 * @param col1 a column from the board
+	 * @param row2 a row from the board
+	 * @param col2 a column from the board
+	 * @param rows the number of rows in the board
+	 * @param columns the number of columns in the board
+	 * @ensures the boolean is true if the rows and columns form a continuous line and the boolean 
+	 * is false if the rows and columns do not form a continuous line
+	 * @return boolean Returns true or false
 	 */
 	public static boolean definesMove (int row1, int col1, int row2, int col2, int rows,
 			int columns) {
@@ -39,6 +45,18 @@ public class Move{
 		return isValid;
 	}
 	
+	/**
+	 * Creates a move with the given data
+	 * 
+	 * @param row1 a row from the board
+	 * @param col1 a column from the board
+	 * @param row2 a row from the board
+	 * @param col2 a column from the board
+	 * @param rows the number of rows in the board
+	 * @param columns the number of columns in the board
+	 * @requires {@code definesMove(row1, col1, row2, col2, rows, columns) is true}
+	 * @ensures to create a move with the given data
+	 */
 	public Move(int row1, int col1, int row2, int col2, int rows, int columns) {
 			this.positions [0] = row1;
 			this.positions [1] = col1;
@@ -47,23 +65,52 @@ public class Move{
 			this.rows = rows;
 			this.columns = columns;
 	}
-	
+
+	/**
+	 * Gives the starting row of the move
+	 * 
+	 * @ensures the integer returns is equal to the number of the row
+	 * @return int Returns the starting row of the move
+	 */
 	public int startRow(){
 		return positions[0];
 	}
 	
+	/**
+	 * Gives the starting column of the move 
+	 * 
+	 * @ensures the integer returns is equal to the number of the column
+	 * @return int Returns the starting column of the move
+	 */
 	public int startColumn() {
 		return positions[1];
 	}
 
+	/**
+	 * Gives the ending row of the move
+	 * 
+	 * @ensures the integer returns is equal to the number of the row
+	 * @return int Returns the ending row of the move
+	 */
 	public int endRow(){
 		return positions[2];
 	}
 
+	/**
+	 * Gives the ending column of the move
+	 * 
+	 * @ensures the integer returns is equal to the number of the column
+	 * @return int Returns the ending column of the move
+	 */
 	public int endColumn(){
 		return positions[3];
 	}
 
+	/**
+	 * Determines the direction of the move
+	 * 
+	 * @return the direction of the move
+	 */
 	public Direction direction(){
 		Direction direction;
 		if ((positions[0] == positions[2]) && (positions[1] != positions[3])){
@@ -80,11 +127,23 @@ public class Move{
 		}
 		return direction;
 	}
-
+	
+	/**
+	 * Gives the number of rows
+	 *
+	 * @ensures the integer returned is equal to the number of rows
+	 * @return int Returns the number of rows
+	 */
 	public int rows(){
 		return rows;
 	}
 
+	/**
+	 * Gives the number of columns
+	 *
+	 * @ensures the integer returned is equal to the number of columns
+	 * @return int Returns the number of columns
+	 */
 	public int columns(){
 		return columns;
 	}
