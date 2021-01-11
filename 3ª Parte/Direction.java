e8263bbb9bae8d38ad0613824e367f3c8926bacb
Gacos11 <74110243+Gacos11@users.noreply.github.com>
2021-01-11 15:49:25 +0000

    1

--- 3ª Parte/Direction.java
+++ 3ª Parte/Direction.java
@@ -1,3 +1,6 @@
+/**
+*The 4 possible directions of a move 
+*/
 public enum Direction {
 	HORIZONTAL, VERTICAL, DIAGONAL_RIGHT, DIAGONAL_LEFT
 }
---
+++ 3ª Parte/PuzzleGenerator.java
@@ -0,0 +1,7 @@
+/**
+ * The {@code PuzzleGenerator} class defines a set of procedures that recreate a
+ * simple version of the game.
+ * 
+ * @author Duarte Ferreira, fc54981
+ * @author Vasco Barros, fc54986
+ */
